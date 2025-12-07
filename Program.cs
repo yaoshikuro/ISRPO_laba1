e067f1232275b86b7cd3bb6960667c66c40024d7
salmanishee <yukivoron@gmail.com>
2025-12-07 21:07:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.VisualBasic;
+//int age = 18;
+//string name = "Anna";
+//int a; // объявление
+//a = 8; // присваивание 
+//string city = "Volgograd" // объявление и инициализация сразу
+
+//byte bit1 = 1;
+//byte bit2 = 100;
+//sbyte bit3 = -101;
+//sbyte bit4 = 102;
+//short n1 = 1;
+//short n2 = 102;
+//ushort n3 = 1;
+//ushort n4 = 102;
+//int integer1 = 10;
+//int integer2 = 0b101;
+//uint integer3 = 10;
+//uint integer4 = 0b101;
+//long number1 = -10;
+//long number2 = 0b101;
+//ulong number3 = 10;
+//ulong number4 = 0b101;
+//bool alive = true;
+//bool isDead = false;
+//float floating1 = 3.14f;
+//float floating2 = 30.6f;
+//double yNumber1 = 3.14
+//double yNumber2 = 36.6;
+//decimal dNumber1 = 4.5m;
+//decimal dNumber2 = 23.56m;
+//char s = 'A';
+//char d = ' ';
+//string hello = "hello";
+//string world = "world"
+//object ob1 = 22;
+//object ob2 = 3.14;
+//object ob3 = "hello world!!!"
+//string name = null
+//Console.WriteLine(name);
+
+//Console.WriteLine(int.MaxValue);
+//Console.WriteLine(int.MinValue);
+//Console.WriteLine(double.MaxValue);
+//Console.WriteLine(double.MinValue);
+
+//Console.WriteLine("vvedite vozrast: ");
+//int age = int.Parse(Console.ReadLine());
+
+//string numStr = "123";
+//string nullStr = null;
+
+//int number1 = Convert.ToInt32(nullStr);
+//int number2 = int.Parse(nullStr);
+
+//int metr1 = 1;
+//System.Int32 metr2 = 1;
+
+//var number = 10;
+//var symbol = 'c';
+//var greeting = "hello";
+
+//int number;
+//number = 300;
+
+//string hello = "привет мир ";
+//Console.WriteLine(hello);
+//Console.WriteLine("Я изучаю С#");
+//Console.WriteLine("пока мир...");
+//Console.WriteLine(36.6);
+
+//string name = "Billy";
+//int age = 48;
+//double height = 1.85;
+//Console.WriteLine($"Имя: {name} возраст: {age} рост: {height}");
+//Console.WriteLine($"Имя: {0} возраст: {1} рост: {2}m", name, age, height);
+
+//string hello = "Hello world";
+//Console.Write(hello);
+//Console.Write(" Я изучаю С#");
+
+//Console.Write("Введите свой имя: ")
+//string? name = Console.ReadLine();
+//Console.WriteLine($"привет "{name});
+
+//zadanie 1
+//string game = "Cyberpunk 2077";
+//int chislo = 8;
+//float pi = 3.14;
+//char bukva = 'a';
+//Console.WriteLine($"моя любимая игра {game}");
+//Console.WriteLine($"моя любимая цифра {chislo}");
+//Console.WriteLine(pi);
+//Console.WriteLine(bukva);
+
+//zadanie 2
+//Console.WriteLine("I need more power!!!");
+//Console.WriteLine("I need more power!!!");
+//Console.WriteLine("I need more power!!!");
+//Console.WriteLine("I need more power!!!");
+
+//zadanie 3
+//Console.WriteLine($"\"Hello There\"");
+
+//zadanie 4
+//Console.WriteLine("введите стоимость системного блока:");
+//int block = int.Parse(Console.ReadLine());
+//Console.WriteLine("введите стоимость монитора:");
+//int monik = int.Parse(Console.ReadLine());
+//Console.WriteLine("введите стоимость клавиатуры:");
+//int klava = int.Parse(Console.ReadLine());
+//Console.WriteLine("введите стоимость мыши:");
+//int mouse = int.Parse(Console.ReadLine());
+//Console.WriteLine($"стоимость 3 комплектов {block * 3 + monik * 3 + klava * 3 + mouse * 3}");
+
+//zadanie 5
+//Console.WriteLine("введите значение А");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("введите значение B");
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine(3 * ((a + b) * (a + b) * (a + b)) + 275 * b * b - 127 * a - 41);
+
+//zadaie 6
+//Console.WriteLine("введите имя");
+//string imya = Console.ReadLine();
+//Console.WriteLine("введите возраст");
+//int vozrast = int.Parse(Console.ReadLine());
+//Console.WriteLine($"привет, {imya}");
+//Console.WriteLine($"Сейчас тебе {vozrast} лет");
+//Console.WriteLine($"В следующем году тебе будет {vozrast + 1}");
+
+//zadanie 7
+//Console.WriteLine("введите имя");
+//string imya = Console.ReadLine();
+//Console.WriteLine("введите возраст");
+//int vozrast = int.Parse(Console.ReadLine());
+//Console.WriteLine("введите город");
+//string gorod = Console.ReadLine();
+//Console.WriteLine($"Привет, {imya}! Тебе {vozrast} лет и ты живёшь в городе {gorod}");
+
+//zadanie 8
+//Console.WriteLine("введите градуса Цельсия");
+//int celsia = int.Parse(Console.ReadLine());
+//Console.WriteLine(32 + celsia * 1.8);
